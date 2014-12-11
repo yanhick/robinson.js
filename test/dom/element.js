4e2f81a517b65f013b0292a55d74405aa35acaaf
yannick <yannick.dominguez@gmail.com>
2014-12-11 18:19:21 +0100

    implemented basic element node

---
+++ test/dom/element.js
@@ -0,0 +1,18 @@
+
+var expect = require('chai').expect;
+
+var ElementNode = require('../../lib/dom/element');
+
+describe('element', function () {
+    it('should create a new element node', function () {
+        var elementNode = new ElementNode('div',
+                                          [{foo: 'bar'}],
+                                         []);
+        expect(elementNode).to.eql({
+            children: [],
+            attributes: [{foo: 'bar'}],
+            nodeType: 1,
+            nodeName: 'div'
+        });
+    });
+});
