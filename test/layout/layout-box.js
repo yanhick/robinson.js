a89ec2ccbe488477dc4396ac821f8c45321dfdbb
yannick <yannick.dominguez@gmail.com>
2014-12-18 17:30:27 +0100

    Implemented layout

--- test/layout/layout-box.js
+++ test/layout/layout-box.js
@@ -5,6 +5,10 @@ var Dimensions = require('../../lib/layout/dimensions');
 var Rect = require('../../lib/layout/rect');
 var EdgeSize = require('../../lib/layout/edge-size');
 var LayoutBox = require('../../lib/layout/layout-box');
+var StyledNode = require('../../lib/style/styled-node');
+var ElementNode = require('../../lib/dom/element');
+var Value = require('../../lib/css/value');
+var Unit = require('../../lib/css/unit');
 
 describe('layout box', function () {
     it('should instantiate a layout box', function () {
@@ -13,7 +17,7 @@ describe('layout box', function () {
         var edge = new EdgeSize(10, 10, 10, 10);
         var dimensions = new Dimensions(rect, edge, edge, edge);
 
-        var layoutBox = new LayoutBox(dimensions, boxType, []);
+        var layoutBox = new LayoutBox(boxType, dimensions, []);
         expect(layoutBox.dimensions).to.eql(dimensions);
         expect(layoutBox.boxType).to.eql(boxType);
         expect(layoutBox.children).to.eql([]);
@@ -26,9 +30,133 @@ describe('layout box', function () {
             var edge = new EdgeSize(10, 10, 10, 10);
             var dimensions = new Dimensions(rect, edge, edge, edge);
 
-            var layoutBox = new LayoutBox(dimensions, boxType, []);
+            var layoutBox = new LayoutBox(boxType, dimensions, []);
 
             expect(layoutBox.getStyleNode()).to.eql('foo');
         });
     });
+
+    describe('#calculateBlockHeight', function () {
+        it('should set the actual block height if provided', function () {
+            var element = new ElementNode('div', {foo: 'bar'}, []);
+            var styledNode = new StyledNode(element, {
+                display: 'block',
+                height: new Value().Length(50, Unit.Px())
+            }, []);
+            var boxType = BoxType.Block(styledNode);
+            var layoutBox = createLayoutBox(boxType);
+
+            layoutBox.calculateBlockHeight();
+            expect(layoutBox.dimensions.content.height).to.eql(50);
+        });
+
+        it('should do nothing if height is not explicit');
+    });
+
+    describe('#calculateBlockPosition', function () {
+        it('should set the block position', function () {
+            var edgeSize = new EdgeSize(0, 0, 0, 0);
+            var dimensions = new Dimensions(
+                new Rect(0, 0, 0, 0),
+                edgeSize,
+                edgeSize,
+                edgeSize
+            );
+            var layoutBox = createLayoutBox(undefined, dimensions);
+
+            var containingEdgeSize = new EdgeSize(0, 0, 0, 0);
+            var containingDimensions = new Dimensions(
+                new Rect(10, 20, 0, 0),
+                containingEdgeSize,
+                containingEdgeSize,
+                containingEdgeSize
+            );
+            var containingLayoutBox = createLayoutBox(undefined, containingDimensions);
+            layoutBox.calculateBlockPosition(containingLayoutBox.dimensions);
+
+            expect(layoutBox.dimensions.content.x).to.eql(10);
+            expect(layoutBox.dimensions.content.y).to.eql(20);
+        });
+        it('should set the block position with paddings');
+        it('should set the block position with margins');
+        it('should set the block position with borders');
+    });
+
+    describe('#calculateBlockWidth', function () {
+        it('should set the block width with an explicit width', function () {
+            var edgeSize = new EdgeSize(0, 0, 0, 0);
+            var dimensions = new Dimensions(
+                new Rect(0, 0, 0, 0),
+                edgeSize,
+                edgeSize,
+                edgeSize
+            );
+            var element = new ElementNode('div', {foo: 'bar'}, []);
+            var styledNode = new StyledNode(element, {
+                display: 'block',
+                width: new Value().Length(50, Unit.Px())
+            }, []);
+            var boxType = BoxType.Block(styledNode);
+            var layoutBox = createLayoutBox(boxType, dimensions);
+
+            var containingEdgeSize = new EdgeSize(0, 0, 0, 0);
+            var containingDimensions = new Dimensions(
+                new Rect(0, 0, 0, 100),
+                containingEdgeSize,
+                containingEdgeSize,
+                containingEdgeSize
+            );
+            var containingLayoutBox = createLayoutBox(undefined, containingDimensions);
+
+            layoutBox.calculateBlockWidth(containingLayoutBox.dimensions);
+
+            expect(layoutBox.dimensions.content.width).to.eql(50);
+        });
+
+        it('should set the block width with auto width');
+        it('should set the block width with auto margins');
+    });
+
+    describe('#layoutBlockChildren', function () {
+        it('should layout all its children and set its height from it');
+    });
+
+    describe('#layout', function () {
+        it('should layout itself', function () {
+
+            var element = new ElementNode('div', {foo: 'bar'}, []);
+            var styledNode = new StyledNode(element, {
+                display: 'block',
+                height: new Value().Length(50, Unit.Px())
+            }, []);
+            var boxType = BoxType.Block(styledNode);
+            var layoutBox = createLayoutBox(boxType);
+
+            var containingEdgeSize = new EdgeSize(0, 0, 0, 0);
+            var containingDimensions = new Dimensions(
+                new Rect(0, 0, 0, 0),
+                containingEdgeSize,
+                containingEdgeSize,
+                containingEdgeSize
+            );
+            var containingLayoutBox = createLayoutBox(undefined, containingDimensions);
+
+            layoutBox.layout(containingLayoutBox.dimensions);
+
+            expect(layoutBox.dimensions.content.height).to.eql(50);
+        });
+    });
 });
+
+function createLayoutBox (boxType, dimensions) {
+    var element = new ElementNode('div', {foo: 'bar'}, []);
+    var styledNode = new StyledNode(element, {
+        display: new Value().Keyword('block')
+    }, []);
+    boxType = boxType || BoxType.Block(styledNode);
+    var rect = new Rect(0, 0, 0, 0);
+    var edge = new EdgeSize(10, 10, 10, 10);
+    dimensions = dimensions || new Dimensions(rect, edge, edge, edge);
+
+    return new LayoutBox(boxType, dimensions, []);
+}
---
+++ test/layout/layout-tree.js
@@ -0,0 +1,32 @@
+
+var expect = require('chai').expect;
+var HTMLParser = require('../../lib/html/parser');
+var CSSParser = require('../../lib/css/parser');
+var styleTree = require('../../lib/style/style-tree');
+var layoutTree = require('../../lib/layout/layout-tree');
+var Dimensions = require('../../lib/layout/dimensions');
+var Rect = require('../../lib/layout/rect');
+var EdgeSize = require('../../lib/layout/edge-size');
+
+var util = require('util');
+describe('layout tree', function () {
+    it('should create a layout tree from a styled node', function () {
+
+        var rootElement = new HTMLParser().parse('<html><body></body></html>');
+        var stylesheet = new CSSParser().parse('{display: block;} body {width: 100px; height: 50px;}');
+        var styledTree = styleTree(rootElement, stylesheet);
+
+        laidoutTree = layoutTree(styledTree, new Dimensions(
+            new Rect(0, 0, 200, 100),
+            new EdgeSize(0, 0, 0, 0),
+            new EdgeSize(0, 0, 0, 0),
+            new EdgeSize(0, 0, 0, 0)
+        ));
+
+        expect(laidoutTree.dimensions.content.width).to.eql(200);
+        expect(laidoutTree.dimensions.content.height).to.eql(50);
+
+        expect(laidoutTree.children[0].dimensions.content.width).to.eql(100);
+        expect(laidoutTree.children[0].dimensions.content.height).to.eql(50);
+    });
+});
