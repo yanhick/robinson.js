55a519dcfad2ff687e6465f05447ebb2d12e77e6
yannick <yannick.dominguez@gmail.com>
2014-12-22 03:04:06 +0100

    added test for painting with border

--- test/painting/paint.js
+++ test/painting/paint.js
@@ -33,4 +33,43 @@ describe('paint', function () {
             a: 255
         }]);
     });
+
+    it('should paint layout tree with borders onto a canvas', function () {
+
+        var rootElement = new HTMLParser().parse('<html><body></body></html>');
+        var stylesheet = new CSSParser().parse('{display: block;} body {border-color: #FF0000; border-left-width: 1px; width: 1px; height: 2px;}');
+        var styledTree = styleTree(rootElement, stylesheet);
+
+        laidoutTree = layoutTree(styledTree, new Dimensions(
+            new Rect(0, 0, 2, 2),
+            new EdgeSize(0, 0, 0, 0),
+            new EdgeSize(0, 0, 0, 0),
+            new EdgeSize(0, 0, 0, 0)
+        ));
+
+        var bounds = new Rect(0, 0, 2, 2);
+        var canvas = paint(laidoutTree, bounds);
+
+        expect(canvas.pixels).to.eql([{
+            r: 255,
+            g: 0,
+            b: 0,
+            a: 255
+        }, {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 255
+        }, {
+            r: 255,
+            g: 0,
+            b: 0,
+            a: 255
+        }, {
+            r: 255,
+            g: 255,
+            b: 255,
+            a: 255
+        }]);
+    });
 });
