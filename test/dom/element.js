108cdcb3f8f2ce4e137b6d56cff512f3289d64d5
yannick <yannick.dominguez@gmail.com>
2015-03-07 17:08:38 +0100

    Prepare code for html workshop

--- test/dom/element.js
+++ test/dom/element.js
@@ -6,36 +6,18 @@ var ElementNode = require('../../lib/dom/element');
 describe('element', function () {
 
     describe('#id', function () {
-        it('should return the element id', function () {
-            var elementNode = new ElementNode('div',
-                                              {id: 'my-id'},
-                                             []);
 
-            expect(elementNode.id()).to.eql('my-id');
-        });
-        it('should return null if the element has no id', function () {
-            var elementNode = new ElementNode('div',
-                                              {foo: 'bar'},
-                                             []);
+        it('should return the element id');
 
-            expect(elementNode.id()).to.be.null;
-        });
+        it('should return null if the element has no id');
+
     });
 
     describe('#classes', function () {
-        it('should return the element classes', function () {
-            var elementNode = new ElementNode('div',
-                                              {'class': 'my-class my-other-class'},
-                                             []);
 
-            expect(elementNode.classes()).to.eql(['my-class', 'my-other-class']);
-        });
-        it('should return an empty list if the element has no classes', function () {
-            var elementNode = new ElementNode('div',
-                                              {},
-                                             []);
+        it('should return the element classes');
 
-            expect(elementNode.classes()).to.eql([]);
-        });
+        it('should return an empty list if the element has no classes');
+
     });
 });
--- test/html/index.js
+++ test/html/index.js
@@ -4,60 +4,16 @@ var HTMLParser = require('../../lib/html');
 
 describe('html parser', function () {
 
-    it('should parse <html> tags', function () {
-        var nodes = HTMLParser().parse('<html></html>');
-        expect(nodes.children).to.eql([]);
-        expect(nodes.attributes).to.eql({});
-        expect(nodes.nodeType).to.eql(1);
-        expect(nodes.tagName).to.eql('html');
-    });
+    it('should parse <html> tags');
 
-    it('should parse html with newlines', function () {
-        var nodes = HTMLParser().parse('<html>\n</html>');
-        expect(nodes.children).to.eql([]);
-        expect(nodes.attributes).to.eql({});
-        expect(nodes.nodeType).to.eql(1);
-        expect(nodes.tagName).to.eql('html');
-    });
+    it('should parse html with newlines');
 
-    it('should parse nested tags', function () {
-        var nodes = HTMLParser().parse('<html><body></body></html>');
-        expect(nodes.children[0].tagName).to.eql('body');
-        expect(nodes.children[0].nodeType).to.eql(1);
-        expect(nodes.children[0].children).to.eql([]);
-        expect(nodes.attributes).to.eql({});
-        expect(nodes.nodeType).to.eql(1);
-        expect(nodes.tagName).to.eql('html');
-    });
+    it('should parse text nodes');
 
-    it('should parse text nodes', function () {
-        var nodes = HTMLParser().parse('<html>hello</html>');
+    it('should parse nested tags');
 
-        expect(nodes.children).to.eql([{
-            text: 'hello',
-            nodeType: 3,
-            children: []
-        }]);
-        expect(nodes.attributes).to.eql({});
-        expect(nodes.nodeType).to.eql(1);
-        expect(nodes.tagName).to.eql('html');
-    });
+    it('should parse attributes with quoted values');
 
-    it('should parse attributes with quoted values', function () {
-        var nodes = HTMLParser().parse('<html lang="us"></html>');
-        expect(nodes.children).to.eql([]);
-        expect(nodes.attributes).to.eql({
-            lang: 'us'
-        });
-        expect(nodes.nodeType).to.eql(1);
-        expect(nodes.tagName).to.eql('html');
-    });
+    it('should create a root element if sibling nodes are parsed');
 
-    it('should create a root element if sibling nodes are parsed', function () {
-        var nodes = HTMLParser().parse('<div></div><div></div>');
-        expect(nodes.nodeType).to.eql(1);
-        expect(nodes.tagName).to.eql('html');
-
-        expect(nodes.children.length).to.equal(2);
-    });
 });
