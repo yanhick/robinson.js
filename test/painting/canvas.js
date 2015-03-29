5d18346b8f83e00fd2ab6c81352881f29d3e9c66
yannick <yannick.dominguez@gmail.com>
2015-03-29 17:44:54 +0200

    prepare for painting workshop

--- test/painting/canvas.js
+++ test/painting/canvas.js
@@ -8,20 +8,6 @@ var DisplayCommand = require('../../lib/painting/display-command');
 
 describe('painting', function () {
     describe('#paintItem', function () {
-        it('should paint a display command', function () {
-            var canvas = new Canvas(1, 1);
-
-            canvas.paintItem(new DisplayCommand().SolidColor(
-                new Color(0, 0, 0, 0),
-                new Rect(0, 0, 1, 1)
-            ));
-
-            expect(canvas.pixels).to.eql([{
-                r: 0,
-                g: 0,
-                b: 0,
-                a: 0
-            }]);
-        });
+        it('should paint a display command');
     });
 });
--- test/painting/paint.js
+++ test/painting/paint.js
@@ -10,66 +10,7 @@ var EdgeSize = require('../../lib/layout/edge-size');
 var paint = require('../../lib/painting/paint');
 
 describe('paint', function () {
-    it('should paint the layout tree onto a canvas', function () {
+    it('should paint the layout tree onto a canvas');
 
-        var rootElement = new HTMLParser().parse('<html><body></body></html>');
-        var stylesheet = new CSSParser().parse('{display: block;} body {background: #FF0000; width: 1px; height: 1px;}');
-        var styledTree = styleTree(rootElement, stylesheet);
-
-        var laidoutTree = layoutTree(styledTree, new Dimensions(
-            new Rect(0, 0, 1, 1),
-            new EdgeSize(0, 0, 0, 0),
-            new EdgeSize(0, 0, 0, 0),
-            new EdgeSize(0, 0, 0, 0)
-        ));
-
-        var bounds = new Rect(0, 0, 1, 1);
-        var canvas = paint(laidoutTree, bounds);
-
-        expect(canvas.pixels).to.eql([{
-            r: 255,
-            g: 0,
-            b: 0,
-            a: 255
-        }]);
-    });
-
-    it('should paint layout tree with borders onto a canvas', function () {
-
-        var rootElement = new HTMLParser().parse('<html><body></body></html>');
-        var stylesheet = new CSSParser().parse('{display: block;} body {border-color: #FF0000; border-left-width: 1px; width: 1px; height: 2px;}');
-        var styledTree = styleTree(rootElement, stylesheet);
-
-        var laidoutTree = layoutTree(styledTree, new Dimensions(
-            new Rect(0, 0, 2, 2),
-            new EdgeSize(0, 0, 0, 0),
-            new EdgeSize(0, 0, 0, 0),
-            new EdgeSize(0, 0, 0, 0)
-        ));
-
-        var bounds = new Rect(0, 0, 2, 2);
-        var canvas = paint(laidoutTree, bounds);
-
-        expect(canvas.pixels).to.eql([{
-            r: 255,
-            g: 0,
-            b: 0,
-            a: 255
-        }, {
-            r: 255,
-            g: 255,
-            b: 255,
-            a: 255
-        }, {
-            r: 255,
-            g: 0,
-            b: 0,
-            a: 255
-        }, {
-            r: 255,
-            g: 255,
-            b: 255,
-            a: 255
-        }]);
-    });
+    it('should paint layout tree with borders onto a canvas');
 });
